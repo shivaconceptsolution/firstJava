50c20b2ad4dcaeca73e3bc3d3f9713aebef9bc2a
shiva Concept Solution <50239824+shivaconceptsolution@users.noreply.github.com>
2023-04-23 09:49:14 +0530

    Add files via upload

---
+++ EmpSal.java
@@ -0,0 +1,20 @@
+public class EmpSal
+{
+    public static void main(String args[])
+    {
+          float sal=12000;
+          float pf;
+          if(sal>=15000)
+          {
+             pf = sal*.12F;
+             sal = sal-pf;
+             System.out.println("Total PF Deduction is"+pf); 
+          } 
+
+         System.out.println("Salary is "+sal);
+
+    }
+
+
+
+}
